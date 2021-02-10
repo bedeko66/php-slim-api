f3690f2376e9d7b151f6752f3102dda6317209d6
bedeko66 <bedekovicsl@gmail.com>
2021-02-10 18:44:16 +0000

    *

--- public/index.php
+++ public/index.php
@@ -146,7 +146,7 @@ $app->map(['GET', 'POST', 'OPTIONS'], '/{routes:.+}', function($req, $res) {
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
-            ->withHeader('Access-Control-Allow-Origin', $CLIENT_URL)
+            ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 });
